5a6899b640b35c7c1dfab2a181d93b0cb33fd5a2
Lidia Velicia <lvelirui@myuax.com>
2023-03-31 06:45:32 +0200

    tratamiento excepciones ej1

--- ej-01.py
+++ ej-01.py
@@ -1,6 +1,3 @@
-caracteres_validos = 'abcdefghijklmnñopqrstuvwxyzáéíóúABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚ1234567890!"#$%&/()=?¡¿*+[{]}-_.:,;<>|@^~`'
-
-
 class Palindromo:
     def __init__(self, palabra):
         self.palabra = palabra
@@ -8,9 +5,6 @@ class Palindromo:
         #comprobar que es una cadena de caracteres válida
         if not isinstance(palabra, str):
             raise TypeError('La palabra debe ser una cadena de caracteres')
-        for caracter in palabra:
-            if caracter not in caracteres_validos:
-                raise ValueError('Caracter no válido: ' + caracter)
 
     @staticmethod
     def esPalindromo(cadena):
